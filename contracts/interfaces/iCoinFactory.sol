6479495d383a0768829cad1323cda2b329728a9b
artixv <artixv@163.com>
2024-08-30 16:00:19 +0800

    fix compiler version

--- contracts/interfaces/iCoinFactory.sol
+++ contracts/interfaces/iCoinFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Business Source License 1.1
 // First Release Time : 2024.07.30
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 interface iCoinFactory{
     function createDeAndLoCoin(address token) external returns (address[2] memory _pAndLCoin) ;
 }
--- contracts/lendingManager.sol
+++ contracts/lendingManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Business Source License 1.1
 // First Release Time : 2024.07.30
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.6;
 
 // import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
